0dd2df8fb8b5c37edd499d04dfbf4dc217f56b1f
tifangel <tifanyangelia28@gmail.com>
2021-06-09 16:38:10 +0700

    fix viewpager and indicators fragment detail multiple

--- app/src/main/java/com/dot/baseandroid/menu/list/views/FragmentListDetailMultiple.kt
+++ app/src/main/java/com/dot/baseandroid/menu/list/views/FragmentListDetailMultiple.kt
@@ -12,6 +12,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import androidx.viewpager2.widget.ViewPager2
 import com.dot.baseandroid.R
 import com.dot.baseandroid.databinding.FragmentListDetailMultipleBinding
 import com.dot.baseandroid.menu.list.adapters.DetailMultipleAdapter
@@ -45,26 +46,30 @@ class FragmentListDetailMultiple: Fragment() {
         setupIndicators()
         setCurrentIndicators(0)
 
-//        binding.recyclerViewDetailImage.addOnChildAttachStateChangeListener(
-//
-//        )
+        binding.recyclerViewDetailImage.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                setCurrentIndicators(position)
+            }
+        })
+
     }
 
     fun setupIndicators(){
+        val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        layoutParams.setMargins(15,0,15,0)
         binding.indicatorsListDetail.removeAllViews()
         for (i in listImage.indices){
             var imageView = ImageView(activity?.applicationContext)
+            imageView.layoutParams = layoutParams
             imageView.setImageResource(R.drawable.indicator_inactive)
             binding.indicatorsListDetail.addView(imageView)
         }
     }
 
     fun setCurrentIndicators(index: Int){
-        val layoutParams: LinearLayout.LayoutParams = LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-        layoutParams.setMargins(15,0,15,0)
         for (i in 0 until binding.indicatorsListDetail.childCount){
             val indicatorView = binding.indicatorsListDetail.getChildAt(i) as ImageView
-            indicatorView.layoutParams = layoutParams
             if (i == index){
                 indicatorView.setImageResource(R.drawable.indicator_active)
             }else{
